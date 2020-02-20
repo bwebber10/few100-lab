dfc96211d521cb28a3394b176d31d8def4095769
bwebber10 <61164194+bwebber10@users.noreply.github.com>
2020-02-20 20:50:07 +0000

    Thursday Lab

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,16 @@
 
 import './styles.css';
-console.log('Ready to Party With Some TypeScript!');
 
+const information = document.getElementById('information') as HTMLSpanElement;
+const buttons = document.querySelectorAll('button') as NodeListOf<HTMLButtonElement>;
+
+buttons.forEach(b => b.addEventListener('click', handleClick));
+
+function handleClick() {
+    const that = this as HTMLButtonElement;
+    information.innerText = 'You are tipping ' + that.dataset.percent;
+
+    buttons.forEach((b: HTMLButtonElement) => b.disabled = false);
+    that.disabled = true;
+
+}
